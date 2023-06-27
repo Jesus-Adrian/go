96df9deb0a474a14c40cc008432612e0672bf484
Jesus Adrian Pool Sansores <jesusadrian@MacBook-Air-de-Adrian.local>
2023-06-27 11:08:09 -0500

    Otro commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/jesus-adrian/go/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(1855)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
